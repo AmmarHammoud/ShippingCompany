8f65285ab242da58f104ba57c59289d368a8ef68
abd <alssadiabdalrahman5@gmail.com>
2025-07-16 16:16:04 +0300

    add notification

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\SignUpRequest;
+use App\Http\Requests\SignInRequest;
+use App\Services\AuthService;
+use App\Http\Responses\Response;
+
+class AuthController extends Controller
+{
+    private AuthService $userService;
+
+    public function __construct(AuthService $userService)
+    {
+        $this->userService = $userService;
+    }
+    
+    public function signUp(SignUpRequest $signUpRequest)
+    {
+        try {
+            $data = $this->userService->signup($signUpRequest);
+            return Response::success($data['message'], $data['user']);
+        } catch (Throwable $throwable) {
+            $message = $throwable->getMessage();
+            return Response::error($message);
+        }
+    }
+
+    public function signIn(SignInRequest $signInRequest)
+    {
+        try {
+            $data = $this->userService->signin($signInRequest);
+            return Response::success($data['message'], $data['user']);
+        } catch (Throwable $th) {
+            $message = $th->getMessage();
+            return Response::error($message);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,20 @@
 
 use App\Events\TestPusherEvent;
 use App\Http\Controllers\ShipmentController;
+use App\Http\Controllers\EmailVerificationController;
+use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\ShipmentDriverOfferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/no', function(){
+    return response()->json("fjdoa");
+});
 
 Route::middleware(['auth:sanctum'])->prefix('shipments')->group(function () {
     Route::post('/recipient', [ShipmentController::class, 'storeRecipient']);
@@ -20,3 +27,25 @@ Route::get('offers/my-pending', [ShipmentDriverOfferController::class, 'myPendin
 Route::get('offers', [ShipmentDriverOfferController::class, 'offersByStatus']);
 
 Route::get('shipments/{barcode}/confirm', [ShipmentController::class, 'confirmDelivery']);
+
+
+Route::post('/offers/{shipment}/accept', [ShipmentDriverOfferController::class, 'acceptOffer']);
+Route::post('/offers/{shipment}/reject', [ShipmentDriverOfferController::class, 'rejectOffer']);
+Route::get('/offers/my-pending', [ShipmentDriverOfferController::class, 'myPendingOffers']);
+Route::get('/offers', [ShipmentDriverOfferController::class, 'offersByStatus']);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('signup', 'signUp')->name('user.sign_up');
+    Route::post('signin', 'signIn')->name('user.sign_in');
+});
+
+Route::controller(ResetPasswordController::class)->group(function () {
+    Route::post('forgotPassword', 'forgotPassword')->name('check.email_password');
+    Route::post('checkCode', 'checkCode')->name('check.email_password');
+    Route::post('resetPassword', 'resetPassword')->name('check.email_password');
+});
+
+Route::controller(EmailVerificationController::class)->group(function () {
+    Route::post('verifyEmail', 'verifyEmail')->name('check.email_password');
+    Route::post('resendVerificationCode', 'resendVerificationCode')->name('check.email_password');
+});
